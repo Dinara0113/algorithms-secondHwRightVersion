c8f075ab17367415736105c98b58cb3d1158b915
Dinara0113 <dinarkatttb16@gmail.com>
2024-11-01 16:59:07 +0500

    с

---
+++ algorithms-secondHw/src/main/java/pro/sky/algorithms_secondHw/AlgorithmsSecondHwApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.algorithms_secondHw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmsSecondHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmsSecondHwApplication.class, args);
+	}
+
+}
